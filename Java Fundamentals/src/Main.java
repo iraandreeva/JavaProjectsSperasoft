d773de5814a32a715de31702b8d18c3b7fb65b85
Irusea <iraandreeva@users.noreply.github.com>
2018-11-08 17:31:41 +0400

    ava Fundamentals Practice #1: Find the minimum
    Create the following methods:
    
    the method that takes 2 integer arguments and returns the lowest;
    the method that takes 3 integer arguments and returns the lowest (using the first method);
    the method that takes 4 integer arguments and returns the lowest (using the previous method)

---
+++ Java Fundamentals/src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //System.out.println("Min of 2 integers= " + min2(15, 23));
+        //System.out.println("Min of 3 integers= " + min3(15, 23, 2));
+        //System.out.println("Min of 4 integers= " + min4(15, 23, 2, 1));
+
+        Scanner a = new Scanner(System.in);
+        int numA = a.nextInt();
+        Scanner b = new Scanner(System.in);
+        int numB = b.nextInt();
+        Scanner c = new Scanner(System.in);
+        int numC = c.nextInt();
+        Scanner d = new Scanner(System.in);
+        int numD = d.nextInt();
+        System.out.println("Min of 2 integers= " + min2(numA, numB));
+        System.out.println("Min of 3 integers= " + min3(numA, numB, numC));
+        System.out.println("Min of 4 integers= " + min4(numA, numB, numC, numD));
+
+    }
+    public static int min2 (int a, int b)
+    {
+        int min;
+        if (a < b)
+            min = a;
+        else
+            min = b;
+        return min;
+    }
+
+    public static int min3 (int a, int b, int c)
+    {
+        int min;
+        if (c <= (min2(a, b)))
+            min = c;
+        else
+            min = min2(a, b);
+        return min;
+    }
+
+    public static int min4 (int a, int b, int c, int d)
+    {
+        int min;
+        if (d < min3(a, b, c))
+            min = d;
+        else
+            min = min3(a, b, c);
+        return min;
+    }
+}
