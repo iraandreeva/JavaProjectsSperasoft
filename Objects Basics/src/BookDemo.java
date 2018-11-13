42927f6dd58c4b6bbfdba47f5c00e127bb7f6e6a
Irusea <iraandreeva@users.noreply.github.com>
2018-11-13 16:34:39 +0400

    Objects Basics Practice #1
    Write class Author with the following features:
    
    Fields:
    
    firstName
    lastName
    Constructor to initialize Author class fields.
    Method to print out author’s full name in the console.
    
    Write class Book with the following features:
    
    Fileds:
    
    title for the title of the book;
    author of type Author;
    price for the book price of type double.
    Constructor to initialize Book class fields.
    Method to print out book’s details to the screen
    Write a separate class BookDemo with a main() method creates a Book titled “Developing Java Software” with authors Russel Winderand price 79.75. Prints the Book’s details to the console.

---
+++ Objects Basics/src/BookDemo.java
@@ -0,0 +1,53 @@
+public class BookDemo{
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
+}
+
+class Main {
+
+    bookAuthor author;
+    bookName bookDemo;
+
+    public void run(){
+        author = new bookAuthor("Russel", "Winderand");
+        bookDemo = new bookName("Developing Java Software", author, 79.75);
+        author.print();
+        bookDemo.print();
+    }
+
+    public class bookAuthor {
+
+        String firstName;
+        String lastName;
+
+        public bookAuthor(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public void print() {
+            System.out.println("Book's author = " + firstName + " " + lastName);
+        }
+    }
+
+    public class bookName {
+
+        String bookTitle;
+        bookAuthor author;
+        double bookPrice;
+
+        public bookName(String bookTitle, bookAuthor author, double bookPrice) {
+            this.bookTitle = bookTitle;
+            this.author = author;
+            this.bookPrice = bookPrice;
+        }
+
+        public void print() {
+            System.out.println("Book's name = " + bookTitle);
+            System.out.println("Book's price = " + bookPrice);
+        }
+    }
+}
