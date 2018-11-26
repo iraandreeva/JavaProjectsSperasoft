1eeb1c0947555069993bea53cf1828704362de69
Irusea <iraandreeva@users.noreply.github.com>
2018-11-26 19:52:26 +0400

    Finished 5-6

--- RegularExp/src/PracticeDemo.java
+++ RegularExp/src/PracticeDemo.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,8 +13,11 @@ public class PracticeDemo {
 
         String demo1 = "I love dogs and they Love me! My dogs name is Bulka. She is 10 month old. Bulo4ka likes to Sleep and to eat!";
         String demo2 = "2000";
-        String demo3 = "I love     dogs and they Love me! My dogs   name is Bulka. She is 10 month   old. Bulocika    likes to Sleep and to eat!";
+        String demo3 = "I     love     dogs and  they   Love me";
         String demo4 = "26/11/2018";
+        String demo5 = "Mama mama mila mila ramu ramu";
+        String demo6 = "1123333455qwe667";
+        String demo7 = "Helllo sstupid worldd ever";
 
 
         /*while(true) {
@@ -33,7 +37,7 @@ public class PracticeDemo {
             }
         }*/
 
-
+practice7(demo7);
 
 
 
@@ -60,7 +64,7 @@ public class PracticeDemo {
 
     public static void practice3(String demo) {
 
-        Pattern extraSpace = Pattern.compile("[a-z]+|[A-Z][.]|[A-Z][!]|[a-z][.]|[a-z][!]|[A-z][a-z]+|[A-Z]");
+        Pattern extraSpace = Pattern.compile("\\w+|\\w+[.]|\\w+[!]");
         Matcher matcher = extraSpace.matcher(demo);
 
         while (matcher.find()) {
@@ -76,8 +80,58 @@ public class PracticeDemo {
     }
 
 
+
+    //Search for the same words in the text and delete duplicates.
+    //String: "Regular regular popular expressions entered popular use from 1968 1968 1968"
+    //Expected result: "Regular popular expressions entered popular use from 1968"
+
     public static void practice5(String demo) {
-        
+
+        String[] arr = new String[50];
+        Pattern duplicates = Pattern.compile("\\b(\\w+|\\d+)\\b");
+        Matcher matcher = duplicates.matcher(demo);
+        Set<String> hash = new LinkedHashSet<String>();
+
+        while (matcher.find()) {
+            for (int i = 0; i < matcher.groupCount(); i++) {
+                demo = demo.toLowerCase();
+                arr[i] = demo.substring(matcher.start(), matcher.end()) + " ";
+                hash.add(arr[i]);
+            }
+            }
+
+         System.out.print(hash);
+
+    }
+
+
+    //Search for the two consecutive numbers and replace them with a symbol *
+    //String: "1223"
+    //Expected result: "1*3"
+    public static void practice6(String demo) {//0{2}|1{2}|2{2}|3{2}|4{2}|5{2}|6{2}|7{2}|8{2}|9{2}
+
+        demo = demo.replaceAll("(.)\\1", "*");
+        System.out.print(demo);
+
+    }
+
+
+    //Remove the words with double letters in the string.
+    //String: "password wword word Heeeeeeello"
+    //Expected result: "word"
+    public static void practice7(String demo) {
+
+        /*Pattern extraWord = Pattern.compile("\\b(([A-B](.)\\1)|(([a-b]+)(.)\\1)|\\w+(.)\\1\\w+)\\b");
+        Matcher matcher = extraWord.matcher(demo);
+
+        while (matcher.find()) {
+            //demo = demo.substring(matcher.start(), matcher.end());
+            System.out.print(demo.substring(matcher.start(), matcher.end()) + " ");
+        }*/
+
+
+        System.out.print(demo.replaceAll("\\w+(.)\\1\\w+|(.)\\1\\w+", ""));
+
     }
 
 
