f2ce6c7bf9aa5b1261ddb2a4a1093267d25d8bee
Irusea <iraandreeva@users.noreply.github.com>
2018-11-26 20:05:03 +0400

    Update

--- RegularExp/src/PracticeDemo.java
+++ RegularExp/src/PracticeDemo.java
@@ -10,7 +10,6 @@ public class PracticeDemo {
         int num;
         Scanner scan = new Scanner(System.in);
 
-
         String demo1 = "I love dogs and they Love me! My dogs name is Bulka. She is 10 month old. Bulo4ka likes to Sleep and to eat!";
         String demo2 = "2000";
         String demo3 = "I     love     dogs and  they   Love me";
@@ -19,29 +18,53 @@ public class PracticeDemo {
         String demo6 = "1123333455qwe667";
         String demo7 = "Helllo sstupid worldd ever";
 
+        while(true) {
 
-        /*while(true) {
-
-            System.out.println("Enter number of practice:");
+            System.out.println("\n\nEnter number of practice:");
             num = scan.nextInt();
 
             switch (num) {
-                case 1: System.out.println(demo1); practice1(demo1);
-                case 2: System.out.println(demo2); practice2(demo2);
-                case 3: System.out.println(demo3); practice3(demo3);
-                case 4: System.out.println(demo4); practice4(demo4);
-                case 5: System.out.println(demo5); practice4(demo5);
-                case 6: System.out.println(demo6); practice4(demo6);
-                case 7: System.out.println(demo7); practice4(demo7);
-
+                case 1: {
+                    System.out.println("String: " + demo1);
+                    practice1(demo1);
+                    break;
+                }
+                case 2: {
+                    System.out.println("String: " + demo2);
+                    practice2(demo2);
+                    break;
+                }
+                case 3: {
+                    System.out.println("String: " + demo3);
+                    practice3(demo3);
+                    break;
+                }
+                case 4: {
+                    System.out.println("String: " + demo4);
+                    practice4(demo4);
+                    break;
+                }
+                case 5: {
+                    System.out.println("String: " + demo5);
+                    practice5(demo5);
+                    break;
+                }
+                case 6: {
+                    System.out.println("String: " + demo6);
+                    practice6(demo6);
+                    break;
+                }
+                case 7: {
+                    System.out.println("String: " + demo7);
+                    practice7(demo7);
+                    break;}
+                default:
+                {
+                    System.out.println("\nNo such case. ReEnter 1-7 = ");
+                    num = scan.nextInt();
+                }
             }
-        }*/
-
-practice7(demo7);
-
-
-
-
+        }
     }
 
     public static void practice1(String demo) {
@@ -51,7 +74,7 @@ practice7(demo7);
 
         while (matcher.find()) {
             for (int i = 0; i < matcher.groupCount(); i++) {
-                System.out.println(matcher.group(i));
+                System.out.print(matcher.group(i) + " ");
             }
         }
     }
@@ -79,12 +102,6 @@ practice7(demo7);
 
     }
 
-
-
-    //Search for the same words in the text and delete duplicates.
-    //String: "Regular regular popular expressions entered popular use from 1968 1968 1968"
-    //Expected result: "Regular popular expressions entered popular use from 1968"
-
     public static void practice5(String demo) {
 
         String[] arr = new String[50];
@@ -104,36 +121,18 @@ practice7(demo7);
 
     }
 
-
-    //Search for the two consecutive numbers and replace them with a symbol *
-    //String: "1223"
-    //Expected result: "1*3"
-    public static void practice6(String demo) {//0{2}|1{2}|2{2}|3{2}|4{2}|5{2}|6{2}|7{2}|8{2}|9{2}
+    public static void practice6(String demo) {
 
         demo = demo.replaceAll("(.)\\1", "*");
         System.out.print(demo);
 
     }
 
-
-    //Remove the words with double letters in the string.
-    //String: "password wword word Heeeeeeello"
-    //Expected result: "word"
     public static void practice7(String demo) {
-
-        /*Pattern extraWord = Pattern.compile("\\b(([A-B](.)\\1)|(([a-b]+)(.)\\1)|\\w+(.)\\1\\w+)\\b");
-        Matcher matcher = extraWord.matcher(demo);
-
-        while (matcher.find()) {
-            //demo = demo.substring(matcher.start(), matcher.end());
-            System.out.print(demo.substring(matcher.start(), matcher.end()) + " ");
-        }*/
 
 
         System.out.print(demo.replaceAll("\\w+(.)\\1\\w+|(.)\\1\\w+", ""));
 
     }
-
-
 }
 
