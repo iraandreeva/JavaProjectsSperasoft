b561c582826926e14bccb3d68324cf6174835937
Irusea <iraandreeva@users.noreply.github.com>
2018-11-26 23:45:30 +0400

    Finished 7

--- RegularExp/src/PracticeDemo.java
+++ RegularExp/src/PracticeDemo.java
@@ -12,7 +12,7 @@ public class PracticeDemo {
 
         String demo1 = "I love dogs and they Love me! My dogs name is Bulka. She is 10 month old. Bulo4ka likes to Sleep and to eat!";
         String demo2 = "2000";
-        String demo3 = "I     love     dogs and  they   Love me";
+        String demo3 = "I     love    100  dogs and  they   Love me! My dogs      name is Bulka. She is     10 month old. Bulo4ka     likes to Sleep and to eat!";
         String demo4 = "26/11/2018";
         String demo5 = "Mama mama mila mila ramu ramu";
         String demo6 = "1123333455qwe667";
@@ -69,7 +69,7 @@ public class PracticeDemo {
 
     public static void practice1(String demo) {
 
-        Pattern upperCase = Pattern.compile("\\b([A-Z]\\w*?)\\b");
+        Pattern upperCase = Pattern.compile("\\b([A-Z]\\w*)\\b");
         Matcher matcher = upperCase.matcher(demo);
 
         while (matcher.find()) {
@@ -81,31 +81,28 @@ public class PracticeDemo {
 
     public static void practice2(String demo) {
 
-        System.out.print(demo.matches("\\b0*(201[3-9]|2[1-9][0-9][0-9]|20[2-9][0-9]|[3-4][0-9][0-9][0-9]|50[0-3][0-9]|504[0-3])\\b" ));
+        System.out.print(demo.matches("2\\d(1[3-9]|[2-9]\\d)|[3,4]\\d{3}|50([0-3]\\d|4[0-3])" ));
+    //
 
     }
 
     public static void practice3(String demo) {
 
-        Pattern extraSpace = Pattern.compile("\\w+|\\w+[.]|\\w+[!]");
-        Matcher matcher = extraSpace.matcher(demo);
+                System.out.print(demo.replaceAll("\\s+", " "));
 
-        while (matcher.find()) {
-                System.out.print(demo.substring(matcher.start(), matcher.end()) + " ");
-        }
 
     }
 
     public static void practice4(String demo) {
 
-        System.out.print(demo.matches("^([0-2][0-9]|3[0-1])[/](0[0-9]|1[0-2])[/][0-9]{4}"));
+        System.out.print(demo.matches("^([0-2]\\d|3[0-1])[/](0\\d|1[0-2])[/]\\d{4}"));
 
     }
 
     public static void practice5(String demo) {
 
         String[] arr = new String[50];
-        Pattern duplicates = Pattern.compile("\\b(\\w+|\\d+)\\b");
+        Pattern duplicates = Pattern.compile("(\\w+)", Pattern.CASE_INSENSITIVE);
         Matcher matcher = duplicates.matcher(demo);
         Set<String> hash = new LinkedHashSet<String>();
 
@@ -131,7 +128,7 @@ public class PracticeDemo {
     public static void practice7(String demo) {
 
 
-        System.out.print(demo.replaceAll("\\w+(.)\\1\\w+|(.)\\1\\w+", ""));
+        System.out.print(demo.replaceAll("\\w*(.)\\1\\w*", ""));
 
     }
 }
