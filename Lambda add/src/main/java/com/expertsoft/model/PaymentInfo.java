cf432956327a10f26ea3355aba644e01a7f53c29
Irusea <iraandreeva@users.noreply.github.com>
2018-11-29 17:22:58 +0400

    Lambda additional

---
+++ Lambda add/src/main/java/com/expertsoft/model/PaymentInfo.java
@@ -0,0 +1,41 @@
+package com.expertsoft.model;
+
+import java.util.function.Function;
+
+public class PaymentInfo {
+
+    public enum CardType {
+        VISA,
+        MASTERCARD
+    }
+
+    public PaymentInfo() {}
+
+    private CardType cardType;
+    private String cardNumber;
+    private String securityCode;
+
+    public CardType getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(CardType cardType) {
+        this.cardType = cardType;
+    }
+
+    public String getSecurityCode() {
+        return securityCode;
+    }
+
+    public void setSecurityCode(String securityCode) {
+        this.securityCode = securityCode;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+}
